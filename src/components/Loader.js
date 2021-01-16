a1397c8cd031e02977375758191ddcbcf921a6fa
Eduard Podvoiskyi <edik@atlas.in.ua>
2021-01-16 23:53:09 +0100

    just one more time first commit

---
+++ src/components/Loader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Loader = () => {
+    return (
+        <div className="text-center">
+            <div className="spinner-border text-primary" role="status">
+                <span className="rs-only">Loading...</span>
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react'
+
+
+export const About = () => {
+    
+        return (
+            <Fragment>
+                <div class="jumbotron">
+                    <h1 class="display-4">Best React Note App</h1>
+                    <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                    
+                    <p class="lead">
+                        App version: <strong>1.0.4</strong>
+                    </p>
+                </div>
+            </Fragment>
+        )
+    
+    
+}
